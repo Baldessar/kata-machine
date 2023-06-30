fc51f15a56e5ce4a879ab3855907dc8893a79136
baldessar <gb.baldessar@gmail.com>
2023-06-30 08:10:07 -0300

    Add two crystal balls solution algorithm

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jumpSize = Math.floor(Math.sqrt(breaks.length))
+
+    let doesBreak = false
+    let floor = 0
+
+    while(!doesBreak) {
+        floor += jumpSize
+        doesBreak = breaks[floor]
+    }
+
+    let breakingFloor = -1
+    for (let i = floor-jumpSize; i<=floor; i++){
+        if(breaks[i]) {
+            breakingFloor = i
+            break
+        }
+    }
+
+    return breakingFloor
 }
