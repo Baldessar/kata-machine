20ec7363c203deab58b1979316c64f0eb0dec298
baldessar <gb.baldessar@gmail.com>
2023-10-10 15:05:05 -0300

    Add binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,23 @@
+
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let high = haystack.length
+    let low = 0
+    
+    while (high > low) {
+        const mid = Math.floor(low + (high- low)/2)
+        const value = haystack[mid]
+
+        if(value === needle) {
+            return true
+        }
+        else if (value < needle) {
+            low = mid + 1
+        } else if(value > needle) {
+            high = mid
+        }
+    }
+
+
+    return false
 }
